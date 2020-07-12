03b8039cf95ae544b95685308ad5724c0bd3d6b1
ana-prathap <prathap@anakinmedia.com>
2020-07-12 11:46:15 +0530

    pf src

--- next.config.js
+++ next.config.js
@@ -2,17 +2,23 @@
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
 
-const debug = process.env.NODE_ENV !== "production";
+// Ref LINK : https://github.com/vercel/next.js/issues/9753
+
+const repoNameURIPrefix =
+  process.env.NODE_ENV === 'production' ? '/me' : '';
 
 module.exports = {
+  generateBuildId: async () => 'current',
   exportPathMap: function () {
     return {
       "/": { page: "/" },
       "/about": { page: "/about" },
     }
   },
-  //assetPrefix: '',
-  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+  assetPrefix: repoNameURIPrefix,
+  env: {
+    LINK_PREFIX: repoNameURIPrefix,
+  },
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,6 @@ import Link from 'next/link'
 export default () => (
   <div>
     <div>About us</div>
-    <div>Back to <Link href='/' as={ process.env.BACKEND_URL + '/'}><a>Home</a></Link></div>
+    <div>Back to <Link href='/' as={ process.env.LINK_PREFIX + '/'}><a>Home</a></Link></div>
   </div>
 )
