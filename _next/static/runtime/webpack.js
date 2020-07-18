702559096e20b78d5d3cbb54d9ac46334591018b
ana-prathap <prathap@anakinmedia.com>
2020-07-18 22:43:15 +0530

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0277f77e0f852b3d986f";
+/******/ 	var hotCurrentHash = "605b1168486ae06054d1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
