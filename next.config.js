79dfec3e2924b712b8f8579a0dac7d6c3fa56a0c
prathap <prathap@anakinmedia.com>
2021-01-30 19:54:51 +0530

    skills colors

--- next.config.js
+++ next.config.js
@@ -49,6 +49,8 @@ module.exports = withLess({
   exportPathMap: function () {
     return {
       "/": { page: "/" },
+      "/skills": { page: "/skills" },
+      "/projects": { page: "/projects" },
     }
   },
   assetPrefix: repoNameURIPrefix,
